904165ed0a6ce3d5a6120154d2f45fdbf6a7b30d
carolinca107387 <carolinca107387@th-nuernberg.de>
2026-07-07 17:03:37 +0200

    Added CP solver for full Benchmark Evaluation

---
+++ Solvers/CPBenchmarkEvaluation/CPBenchmarkApplication.cs
@@ -0,0 +1,54 @@
+﻿using JobShopSchedulingFramework.Results;
+
+
+namespace JobShopSchedulingFramework.Solvers.CPBenchmarkEvaluation
+{
+    public static class CpBenchmarkApplication
+    {
+        public static void Run(string folder)
+        {
+            var files =
+                Directory.GetFiles(folder, "*.txt")
+                         .OrderBy(x => x)
+                         .ToList();
+
+            string outputPath =
+    Path.Combine(
+        GetProjectRootFolder(),
+        "Results",
+        "Csv",
+        "cp_benchmark.csv");
+
+
+            Directory.CreateDirectory(
+                Path.GetDirectoryName(outputPath)!);
+
+
+            CpBenchmarkCsvWriter.WriteHeader(
+                outputPath);
+
+
+            var results =
+                CpBenchmarkRunner.RunBatch(
+                    files,
+                    timeLimitSeconds: 90,
+                    outputPath);
+
+
+            Console.WriteLine("CP benchmark CSV written to:");
+            Console.WriteLine(outputPath);
+
+            Console.WriteLine("CP benchmark CSV written to:");
+            Console.WriteLine(outputPath);
+        }
+
+
+        private static string GetProjectRootFolder()
+        {
+            return Path.GetFullPath(
+                Path.Combine(
+                    AppContext.BaseDirectory,
+                    @"..\..\.."));
+        }
+    }
+}
